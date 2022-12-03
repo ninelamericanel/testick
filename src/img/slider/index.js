1d01cab3cd43415ccb35c5ce0ff163abfa628fcf
ninelamericanel <ninelamericanel@gmail.com>
2022-12-03 22:05:50 +0300

    added fonts and imgs

---
+++ src/img/slider/index.js
@@ -0,0 +1,12 @@
+import photoScroll1 from './1-8.png'
+import photoScroll2 from './1-1.png'
+import photoScroll3 from './1-2.png'
+import photoScroll4 from './1-3.png'
+import photoScroll5 from './1-4.png'
+import photoScroll6 from './1-5.png'
+import photoScroll7 from './1-6.png'
+import photoScroll8 from './1-7.png'
+
+const arrayOfPhoto = [photoScroll1, photoScroll2, photoScroll3, photoScroll4, photoScroll5, photoScroll6, photoScroll7, photoScroll8]
+
+export default arrayOfPhoto
