c9ae7144304f9179afc451927854d47d79613912
ninelamericanel <ninelamericanel@gmail.com>
2022-12-03 22:16:02 +0300

    created footer comp and add stub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <>
-    <Header />
-    </>
-  );
+    const title = 'ut aliquip ex ea commodo consequat'.toUpperCase()
+    const blockTitle = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    const text = 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+
+
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+const Footer = () => {
+
+    return (<footer className="footer">
+        <div className="max-container">
+            <div className='footer__info'>@ test 2020-2022</div>
+        </div>
+    </footer>)
+}
+
+export default Footer;
