dba7c634ac005bec3649a31e197bfc1eccc4b864
ninelamericanel <ninelamericanel@gmail.com>
2022-12-04 17:03:09 +0300

    add broken scrolling for gallery

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -14,15 +14,15 @@ const Gallery = () => {
         let scrollingElement = horizontalScroll.current;
         let pinWrapWidth = scrollingElement?.offsetWidth
         console.log(pinWrapWidth)
-        let t1 = gsap.timeline();
-        t1.to(scrollingElement, {
+        // let t1 = gsap.timeline();
+        gsap.to(scrollingElement, {
             scrollTrigger: {
                 trigger: element,
-                start: '0 top',
-                end: pinWrapWidth + 1000,
+                start: `top top`,
+                end: pinWrapWidth,
                 pin: true,
                 scrub: true,
-                markers: true
+                markers: true,
             },
             x: -pinWrapWidth + 1000,
             ease: 'none',
@@ -30,10 +30,10 @@ const Gallery = () => {
         })
     }, [])
     return (
-        <div ref={gallery} className="scroll">
-            <div className="container">
+        <div ref={gallery} className="container">
+            {/*<div className="container">*/}
                 <h2 className="title">Title</h2>
-            </div>
+            {/*</div>*/}
             <div className="horizontal-scroll" ref={horizontalScroll}>
                 {arrayOfPhoto.map((src, i) => {
                     return <div key={i} className="gallery__image">
