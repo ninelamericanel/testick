20cb9118564e4eee411c32359ab75c03ed32fffd
ninelamericanel <ninelamericanel@gmail.com>
2022-12-05 12:55:53 +0300

    fixed styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './normolize.css'
 import App from './App';
 
 const rootElement = document.getElementById("root");
