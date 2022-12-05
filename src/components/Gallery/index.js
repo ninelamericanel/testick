b760ca71ac9b68de15ae2f4b7565ae739e220a94
ninelamericanel <ninelamericanel@gmail.com>
2022-12-05 12:38:41 +0300

    clear comments and added checking screen width for correct working scrolltrigger

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,37 +2,33 @@ import {gsap} from "gsap";
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import './styles.css'
 import {useLayoutEffect, useRef} from "react";
+import useWindowDimensions from "../../hook/useWindowDimensions";
 
 const Gallery = ({content}) => {
+    const {width} = useWindowDimensions();
+
     gsap.registerPlugin(ScrollTrigger)
     const gallery = useRef(null)
     const horizontalScroll = useRef(null);
 
     useLayoutEffect(() => {
-        let element = gallery.current;
-        let scrollingElement = horizontalScroll.current;
-        let pinWrapWidth = scrollingElement?.offsetWidth
-        console.log(element)
-        // let t1 = gsap.timeline();
-        gsap.to(scrollingElement, {
-            scrollTrigger: {
-                trigger: element,
-                //`блок рабочаяОбласть`
-                start: `center center`,
-                end: `80% center`,
-                // end: pinWrapWidth,
-                //закрепляется на самой галерее пока она не прокрутится
-                pin: true,
-                //задает плавность, в течении этого значения после остановки скролла
-                //идет анимация
-                scrub: 2,
-                // markers: true,
-            },
-            xPercent: -120,
-            duration: 90,
-            ease: 'none',
+        if (width >= 1024) {
+            let element = gallery.current;
+            let scrollingElement = horizontalScroll.current;
+            gsap.to(scrollingElement, {
+                scrollTrigger: {
+                    trigger: element,
+                    start: `center center`,
+                    end: `80% center`,
+                    pin: true,
+                    scrub: 2,
+                },
+                xPercent: -120,
+                duration: 90,
+                ease: 'none',
 
-        })
+            })
+        }
     }, [])
     return (
         <div ref={gallery} className="gallery__section">
