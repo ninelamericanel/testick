83a738764ae170b63db01e5121f57de9fa8fd1bc
ninelamericanel <ninelamericanel@gmail.com>
2022-12-05 10:17:58 +0300

    added changing header's element if screen width < 1024px

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,27 @@
 import logo from "../../img/icon/icon.svg";
 import logoName from "../../img/icon/Circeya.svg";
+import iconTel from '../../img/icon/fluent_call-20-regular.svg'
 import './styles.css';
+import useWindowDimensions from "../../hook/useWindowDimensions";
 
 const Header = () => {
+    const {width} = useWindowDimensions();
     return (
         <header className="header">
             <div className="max-container">
                 <div className="header__content">
-                    <div className="logo">
-                        <img src={logo} />
-                        <img src={logoName} />
+                    <a href="#" className="header__logo">
+                        <img src={logo}/>
+                        <img src={logoName}/>
+                    </a>
+                    <div className="header__info">
+                        {width >= 1024 ?
+                            <button className="header__button" type="button">+7(495) 495-49-54</button> :
+                            <button className="header__button" type="button">
+                                <img src={iconTel} alt="contact"/>
+                            </button>
+                        }
                     </div>
-                    <div className="info">+7 (495) 495-49-54</div>
                 </div>
             </div>
         </header>
