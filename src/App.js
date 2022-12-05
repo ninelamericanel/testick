d3afa3bc7f473fc54542f3980a6a586f3dbabf18
ninelamericanel <ninelamericanel@gmail.com>
2022-12-05 11:31:22 +0300

    created content data and refactored app comp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
-import './App.css';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Section from "./components/Section";
 import Gallery from "./components/Gallery";
-import photo1 from "./img/julia-d-FlNTu2Bj4Dg-unsplash-1.png";
-import photo2 from "./img/taisiia-stupak-viq7xx1boxo-unsplash1-2.png";
-import photo3 from './img/julia-d-FlNTu2Bj4Dg-unsplash.png'
-import photo4 from './img/taisiia-stupak-viq7xx1boxo-unsplash-2.png'
+import { data } from './contentData'
+import './App.css';
 
 function App() {
-    const title = 'ut aliquip ex ea commodo consequat'.toUpperCase()
-    const blockTitle = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
-    const text1 = 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-    const text2 = 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
-
     return (
         <>
             <Header/>
             <div className="main-wrapper">
                 <section className='section start'>
-                    <Section title={title} text={text1} text2={text2} blockTitle={blockTitle} photo1={photo1} photo2={photo2}/>
+                    <Section content={data.content1}/>
                 </section>
                 <section className="gallery section">
-                    <Gallery title={title}/>
+                    <Gallery content={data.gallery}/>
                 </section>
                 <section className='section end'>
-                    <Section title={title} text={text1} text2={text2} blockTitle={blockTitle} photo1={photo3}
-                             photo2={photo4}/>
+                    <Section content={data.content2}/>
                 </section>
             </div>
             <Footer/>
---
+++ src/contentData/index.js
@@ -0,0 +1,48 @@
+import photo1 from "../img/julia-d-FlNTu2Bj4Dg-unsplash-1.png";
+import photo2 from "../img/taisiia-stupak-viq7xx1boxo-unsplash1-2.png";
+import photo3 from '../img/julia-d-FlNTu2Bj4Dg-unsplash.png'
+import photo4 from '../img/taisiia-stupak-viq7xx1boxo-unsplash-2.png'
+import arrayOfPhoto from "../img/slider";
+
+export const data = {
+    content1: {
+        block1: {
+            title: 'ut aliquip ex ea commodo consequat'.toUpperCase(),
+            subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            src: photo1,
+            text: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' +
+                ' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            text2: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+        },
+        block2: {
+            title: 'ut aliquip ex ea commodo consequat'.toUpperCase(),
+            subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            src: photo2,
+            text: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' +
+                ' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            text2: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+        }
+    },
+    gallery: {
+        title: 'ut aliquip ex ea commodo consequat'.toUpperCase(),
+        photos: arrayOfPhoto
+    },
+    content2: {
+        block1: {
+            title: 'ut aliquip ex ea commodo consequat'.toUpperCase(),
+            subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            src: photo3,
+            text: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' +
+                ' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            text2: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+        },
+        block2: {
+            title: 'ut aliquip ex ea commodo consequat'.toUpperCase(),
+            subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            src: photo4,
+            text: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' +
+                ' ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            text2: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+        }
+    },
+}
