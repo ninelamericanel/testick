9a95446554a20288a4418b2ce9906f2538853c58
ninelamericanel <ninelamericanel@gmail.com>
2022-12-05 11:28:03 +0300

    refactored props

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,33 +1,33 @@
 import './styles.css'
 
-const Section = ({text2, title, blockTitle, text, photo1, photo2}) => {
+const Section = ({content: {block1, block2}}) => {
     return (
         <section className="section">
             <div className="container section__wrapper">
-                <h2 className="main__title title">{title}</h2>
+                <h2 className="main__title title">{block1.title}</h2>
                 <div className="wrapper">
                     <div className="section__block outside">
                         <div className="section__image">
-                            <img src={photo1}/>
+                            <img src={block1.src}/>
                         </div>
                         <div className="section__description">
-                            <h3>{blockTitle}</h3>
+                            <h3>{block1.subtitle}</h3>
                             <div className="section__text">
-                                <p>{text}</p>
-                                <p>{text2}</p>
+                                <p>{block1.text}</p>
+                                <p>{block1.text2}</p>
                             </div>
                         </div>
                     </div>
                     <div className="section__block">
                         <div className="section__description">
-                            <h3>{blockTitle}</h3>
+                            <h3>{block2.subtitle}</h3>
                             <div className="section__text">
-                                <p>{text}</p>
-                                <p>{text2}</p>
+                                <p>{block2.text}</p>
+                                <p>{block2.text2}</p>
                             </div>
                         </div>
                         <div className="section__image">
-                            <img src={photo2}/>
+                            <img src={block2.src}/>
                         </div>
                     </div>
                 </div>
